5dddc31c2fa95761722d05a3dcf3bddac5b5d951
Clint Favor <clint.favor@mllrdev.com>
2018-01-25 01:01:33 +0800

    skadoosh pricing page no slider yet hehe xd page work as intendend :) with static data

---
+++ views/pricing/pricing.php
@@ -0,0 +1,309 @@
+<!--Template Name: rovan
+File Name: index.html
+Author Name: ThemeVault
+Author URI: http://www.themevault.net/
+License URI: http://www.themevault.net/license/-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="../../images/icons/favicon.png"/>
+        <title>Sean Global Skrrrr</title>
+        <link href="../../css/pricing.css" rel="stylesheet" media="all">
+        <link href="../../css/scrolling-nav.css" rel="stylesheet" media="all">
+        <!-- Bootstrap core CSS -->
+        <link href="../../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../../font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Custom styles for this template -->
+        <link href="../../css/magnific-popup.css" rel="stylesheet">
+        <link href="../../css/pricing.css" rel="stylesheet">
+        <link rel="stylesheet" href="../../css/lightbox.min.css">
+        <link href="../../css/responsive.css" rel="stylesheet">
+        <script src="../../js/jquery.min.js" type="text/javascript"></script>
+        <script src="../../js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="../../js/scrolling-nav.js" type="text/javascript"></script>
+        <script src="../../js/counterup.min.js" type="text/javascript"></script>
+        <script src="../../js/waypoints.min.js" type="text/javascript"></script>
+        <script src="../../js/wow.min.js" type="text/javascript"></script>
+        <script src="../../js/mousescroll.js" type="text/javascript"></script>
+        <script src="../../js/jquery.inview.min.js" type="text/javascript"></script>
+        <script src="../../js/magnific-popup.js" type="text/javascript"></script>
+        <script src="../../js/custom.js" type="text/javascript"></script>
+
+        <!-- JQUery UI -->
+        <link href="../../css/jquery-ui.min.css">
+        <link href="../../css/jquery-ui.structure.min.css">
+        <link href="../../css/jquery-ui.theme.min.css">
+        <script src="../../js/jquery-ui.js"></script>
+    </head>
+    <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+        <nav class="navbar navbar-default navbar-fixed-top custom-nav-wrapper">
+            <div class="container">
+                <div class="col-md-12 col-sm-10 mobile-space-remove">
+                    <div class="navbar-header">
+                        <button data-target="#myNavbar" data-toggle="collapse" class="navbar-toggle" type="button">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>                        
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+
+
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#myPage"><div class="home-icon"><i class="fa fa-home" aria-hidden="true"></i></div></a></li>
+                           <!--  <li><a href="#about">about</a></li>
+                            <li><a href="#skill">skill</a></li>
+                            <li><a href="#professional">professional</a></li>
+                            <li><a href="#services">services</a></li> -->
+                            <li><a href="#features">features</a></li>
+                            <li><a href="#portfolio">portfolio</a></li>
+                            <li><a href="#pricing">pricing</a></li>
+                            <li class="nav-logo-container"><div class="nav-logo"><a href="#myPage"><img src="../../images/logo-mid.png"></a></div></li>
+                            <!-- <li><a href="#portfolio">portfolio</a></li>
+                            <li><a href="#testimonials">testimonials</a></li>
+                            <li><a href="#contact">contact us</a></li> -->
+                            <li><a href="#resources">resources</a></li>
+                            <li><a href="views/blog/blog.html">blog</a></li>
+                            <li><a href="#help">help</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
+        <div class="pricing-banner-full-image">
+
+            <div class="banner-content">
+                <div class="col-set">
+                    <div class="left">
+                        <div class="title">
+                            <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+
+        <section id="services-block" class="">
+            <div>
+                <div class="col-set">
+                    <div class="col col-lg-3 col-md-3 col-sm-12 pricing-navigation-list">
+                        <ul>
+                            <li class="manage-unit-navigation active">
+                                <h3><span id="manage-unit">MANAGE UNIT</span></h3>
+                            </li>
+                            <li class="additional-service-navigation">
+                                <h3><span id="additional-service">ADDITIONAL SERVICES AVAILABLE</span></h3>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-9 pricing-content">
+                        <div class="manage-unit-content">
+                            <div class="col-md-12 services-header">
+                                <h3>NUMBER OF UNITS YOU MANAGE</h3>
+                            </div>
+                            <div class="col-md-12 manage-unit-content">
+                                <div id="slider"></div>
+                            </div>
+                        </div>
+                        <div class="services-content" style="display: none;">
+                            <div class="col-md-12 services-header">
+                                <h3>ADDITIONAL SERVICES AVAILABLE</h3>
+                            </div>
+                            <div class="col-md-12 services-content-container">
+                                <div class="col-md-6 services-content">
+                                    <div class="services-content-header">
+                                        <h4>RENTERS INSURANCE</h4>
+                                        <p>Protect yourself and your tenants by requiring renters insurance.</p>
+                                        <p><strong>$14.50</strong> per tenant per month</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 services-content">
+                                    <div class="services-content-header">
+                                        <h4>RENTERS INSURANCE</h4>
+                                        <p>Protect yourself and your tenants by requiring renters insurance.</p>
+                                        <p><strong>$14.50</strong> per tenant per month</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 services-content">
+                                    <div class="services-content-header">
+                                        <h4>RENTERS INSURANCE</h4>
+                                        <p>Protect yourself and your tenants by requiring renters insurance.</p>
+                                        <p><strong>$14.50</strong> per tenant per month</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 services-content">
+                                    <div class="services-content-header">
+                                        <h4>RENTERS INSURANCE</h4>
+                                        <p>Protect yourself and your tenants by requiring renters insurance.</p>
+                                        <p><strong>$14.50</strong> per tenant per month</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="newsletter">
+            <div class="container col-lg-12">
+                <div class="col-md-3"></div>
+                <div class="col-md-4">
+                    <h3>SUBSCRIBE TO OUR NEWSLETTER TO RECEIVE UPDATES.</h3>
+                </div>
+                <div class="col-md-5">
+                    <form action="index.html" method="post" class="subscribe-form">
+                        <div class="field"><input type="email" name="email" class="subscribe-input" placeholder="Email address" autofocus></div>
+                        <div class="button"><button type="submit" class="subscribe-submit">Subscribe Now</button></div>
+                    </form>
+                </div>
+            </div>
+        </section>
+        <footer style="float: left; width: 100%;">
+            <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+                <i class="fa fa-chevron-up"></i>
+            </a>
+            <div class="container">
+                <div class="col-md-12 footer-items">
+                    <div class="col-md-3 item">
+                        <p>Resources</p>
+                        <ul>
+                            <li><a href="#">Cost Saving Calculator</a></li>
+                            <li><a href="#">Property Management</a></li>
+                            <li><a href="#">Laws</a></li>
+                            <li><a href="#">Case Studies</a></li>
+                            <li><a href="#">Guides and Reports</a></li>
+                            <li><a href="">Infographics</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 item">
+                        <p>Partnership</p>
+                        <ul>
+                            <li><a href="#">Affiliate Program</a></li>
+                            <li><a href="#">Referral Program</a></li>
+                            <li><a href="#">Partners</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 item">
+                        <p>About</p>
+                        <ul>
+                            <li><a href="#">Our Story</a></li>
+                            <li><a href="#">News & Events</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Security Policy</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 item">
+                        <p>About</p>
+                        <ul>
+                            <li><a href="#">877-396-7876</a></li>
+                            <li><a href="#">Mon-Fri: 9AM to 6PM EOT</a></li>
+                            <li><a href="#">Knoweledge Base</a></li>
+                            <li><a href="#">Contact Customer Care</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="social-grid">
+                    <ul class="list-unstyled text-center">
+                        <li><a class="facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a class="twitter"><i class="fa fa-twitter"></i></a></li>
+                        <li><a class="insta"><i class="fa fa-instagram"></i></a></li>
+                        <li><a class="google-plus"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a class="youtube"><i class="fa fa-youtube-play"></i></a></li>
+                    </ul>
+                </div>
+                <div class="copyright offspace-35">
+                    &copy; 2017 All right reserved. Designed by <a href="http://www.facebook.com/" target="_blank">Unknown</a>
+                </div>
+            </div>
+        </footer>
+
+        <script>
+            $(document).ready(function () {
+                // Initialize Tooltip
+                $('[data-toggle="tooltip"]').tooltip();
+
+                // Add smooth scrolling to all links in navbar + footer link
+                $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+
+                    // Make sure this.hash has a value before overriding default behavior
+                    if (this.hash !== "") {
+
+                        // Prevent default anchor click behavior
+                        event.preventDefault();
+
+                        // Store hash
+                        var hash = this.hash;
+
+                        // Using jQuery's animate() method to add smooth page scroll
+                        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+                        $('html, body').animate({
+                            scrollTop: $(hash).offset().top
+                        }, 900, function () {
+
+                            // Add hash (#) to URL when done scrolling (default click behavior)
+                            window.location.hash = hash;
+                        });
+                    } // End if
+                });
+            })
+        </script>
+
+        <script>
+            jQuery(function ($) {
+
+                //Initiat WOW JS
+                new WOW().init();
+                //smoothScroll
+                smoothScroll.init();
+
+                // Progress Bar
+                $('#skill').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
+                    if (visible) {
+                        $.each($('div.progress-bar'), function () {
+                            $(this).css('width', $(this).attr('aria-valuetransitiongoal') + '%');
+                        });
+                        $(this).unbind('inview');
+                    }
+                });
+            });
+        </script>
+
+        <script>
+        $(function() {
+            $('.pricing-navigation-list ul li').on('click', function() {
+                $(this).parent().find('li.active').removeClass('active');
+                $(this).addClass('active');
+                
+            });
+            $('.manage-unit-navigation').on('click', function(){
+                $('.manage-unit-content').css('display','inline-block');
+                $('.services-content').css('display', 'none');
+            });
+            $('.additional-service-navigation').on('click', function(){
+                $('.manage-unit-content').css('display','none');
+                $('.services-content').css('display', 'inline-block');
+            });
+        });
+        $("#slider").slider({
+            change: function() {
+                var value = $("#slider").slider("option","value");
+                $("#slider").find(".ui-slider-handle").text(value);
+            },
+            slide: function() {
+                var value = $("#slider").slider("option","value");
+                $("#slider").find(".ui-slider-handle").text(value);
+            }
+        });
+        </script>
+    </body>
+
+</html>
